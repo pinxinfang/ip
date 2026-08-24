4aaff0c02895f5f970e2082340ae93187970b93f
pinxin <E1526026@u.nus.edu>
2026-08-24 14:59:17 +0800

    Add Level-0 greeting and Mochi chatbot identity

---
+++ src/main/java/Mochi.java
@@ -0,0 +1,13 @@
+/**
+ * Runs the Mochi chatbot.
+ */
+public class Mochi {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Mochi.");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
